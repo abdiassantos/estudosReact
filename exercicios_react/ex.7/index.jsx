8b37b88dcb4e90235e736b633ddb095edd54b13a
Abdias Viana <abdviana@gmail.com>
2019-02-27 12:17:30 -0300

    Estudos React

--- exercicios_react/ex.7/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
-
-ReactDOM.render(
-    <ClassComponent value={10} />
-, document.getElementById('app'))
--- exercicios_webpack/ex.2/index.js
+++
@@ -1,4 +0,0 @@
-import Pessoa from './pessoa'
-
-const pessoa = new Pessoa('Abdias Viana dos Santos Neto')
-console.log(pessoa.toString())
